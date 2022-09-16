932cad7b827f6b352c98b8d3e1e6328c6be4edb7
ryudai <bigdragon.06102001@gmail.com>
2022-09-16 14:28:05 +0900

    構文2完成

---
+++ syntax/syntax2/script.js
@@ -0,0 +1,33 @@
+// 数字の足し算
+console.log("---数字の足し算---");
+{
+  const num1 = 10;
+  const num2 = 20;
+  console.log(num1 + num2); // 30
+}
+
+// 文字の足し算
+console.log("---文字の足し算---");
+{
+  const str1 = "10";
+  const str2 = "20";
+  console.log(str1 + str2); // "1020"
+}
+
+// 数字と文字の足し算
+console.log("---数字と文字の足し算---");
+{
+  const num = 10;
+  const str = 20;
+  // console.log(num + str); // ?
+  // console.log(num + String(str)); // ?
+}
+
+// 【発展】テンプレートリテラル
+console.log("---【発展】テンプレートリテラル---");
+{
+  const num = 10;
+  const str = 20;
+  console.log("数字:" + num + "、 文字:" + str); // 普通の結合方法
+  // console.log(`数字:${num}、文字:${str}`); // ?
+}
